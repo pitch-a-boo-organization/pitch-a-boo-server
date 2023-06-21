47802cc79233f117c58f8c55a265a9836ad45486
ThiagoHBA <thiagohhab@gmail.com>
2023-06-21 10:12:27 -0300

    writing game states

---
+++ Sources/PitchABooServer/Entities/GameStages.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thiago Henrique on 21/06/23.
+//
+
+import Foundation
+
+enum GameStages: Int {
+    case first = 10
+    case second = 20
+    case firstStageOfShift = 31
+    case secondStageOfShift = 32
+    case thirdStageOfShift = 33
+    case fourthStageOfShift = 34
+    case fifthStageOfShift = 35
+}
