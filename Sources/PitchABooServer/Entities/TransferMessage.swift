0a316f76e09f6b511863ce177a3a1c97f7b9f433
ThiagoHBA <thiagohhab@gmail.com>
2023-06-24 00:53:49 -0300

    transfer message as public

--- Sources/PitchABooServer/Entities/TransferMessage.swift
+++ Sources/PitchABooServer/Entities/TransferMessage.swift
@@ -12,12 +12,18 @@ public struct TransferMessage: Codable {
     let device: Device
     let message: Data
     
+    public init(code: Int, device: Device, message: Data) {
+        self.code = code
+        self.device = device
+        self.message = message
+    }
+    
     func encodeToTransfer() throws -> Data {
         return try JSONEncoder().encode(self)
     }
 }
 
-enum Device: Int, Codable {
+public enum Device: Int, Codable {
     case iOS = 1
     case tvOS = 2
     case coreOS = 3
