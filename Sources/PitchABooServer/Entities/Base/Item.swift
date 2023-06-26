0daf301b7e87bf3b8f23968700cee2eed18c1434
Joan Wilson <joanwmoliveira@gmail.com>
2023-06-26 17:36:55 -0300

    refactor: add characteristic on Item

--- Sources/PitchABooServer/Entities/Base/Item.swift
+++ Sources/PitchABooServer/Entities/Base/Item.swift
@@ -11,15 +11,16 @@ public struct Item: Codable {
     public let id: Int
     public let name: String
     public let value: Int
+    public let characteristc: Characteristic
 }
 
 extension Item {
     static let availableItems: [Item] = [
-        Item(id: 1, name: "Mad Scientist Brain", value: 6),
-        Item(id: 2, name: "Pen", value: 3),
-        Item(id: 3, name: "Chainsaw Hand", value: 4),
-        Item(id: 4, name: "Pumpkin", value: 3),
-        Item(id: 5, name: "Comfy Socks", value: 5),
-        Item(id: 6, name: "Witch Hat", value: 6),
+        Item(id: 1, name: "Mad Scientist Brain", value: 6, characteristc: .collectionner),
+        Item(id: 2, name: "Pen", value: 3, characteristc: .collectionner),
+        Item(id: 3, name: "Chainsaw Hand", value: 4, characteristc: .extroverted),
+        Item(id: 4, name: "Pumpkin", value: 3, characteristc: .playful),
+        Item(id: 5, name: "Comfy Socks", value: 5, characteristc: .playful),
+        Item(id: 6, name: "Witch Hat", value: 6, characteristc: .extroverted),
     ].shuffled()
 }
