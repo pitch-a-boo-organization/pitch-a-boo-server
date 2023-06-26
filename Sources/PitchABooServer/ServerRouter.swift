ac2a507fb6c37371fba259ff360e475da06225d2
ThiagoHBA <thiagohhab@gmail.com>
2023-06-26 17:55:16 -0300

    removing line of code that remove paused player

--- Sources/PitchABooServer/ServerRouter.swift
+++ Sources/PitchABooServer/ServerRouter.swift
@@ -64,14 +64,13 @@ public class ServerRouter {
                 break
             case .pauseSession:
                 let pauseMessage = try! JSONDecoder().decode(DTOPauseSession.self, from: message.message)
-            guard let playerIndex = session.players.firstIndex(where: { $0.id == pauseMessage.player.id}) else { return }
-                session.pausedPlayers.append(pauseMessage.player)
-                session.players.remove(at: playerIndex)
-                server.sendMessageToAllClients(DefaultMessage.connectedPlayers(session.players).load)
+                    guard let playerIndex = session.players.firstIndex(where: { $0.id == pauseMessage.player.id}) else { return }
+                    session.pausedPlayers.append(pauseMessage.player)
                 case .resumeSession:
                     let resumeMessage = try! JSONDecoder().decode(DTOPauseSession.self, from: message.message)
-                if session.pausedPlayers.contains(where: { $0.id == resumeMessage.player.id}) {
+                    if let playerIndex = session.pausedPlayers.firstIndex(where: { $0.id == resumeMessage.player.id}) {
                         server.connectedClients.append(connection)
+                        session.pausedPlayers.remove(at: playerIndex)
                     }
                 
         }
