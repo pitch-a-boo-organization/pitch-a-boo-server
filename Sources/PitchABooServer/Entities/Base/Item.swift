96513c96fe1f7cbcd0f2abcfa6ce5192696c404a
Joan Wilson <joanwmoliveira@gmail.com>
2023-06-26 18:20:46 -0300

    refactor: update Model

--- Sources/PitchABooServer/Entities/Base/Item.swift
+++ Sources/PitchABooServer/Entities/Base/Item.swift
@@ -7,20 +7,29 @@
 
 import Foundation
 
-public struct Item: Codable {
+import Foundation
+
+public struct Item: Codable, Hashable {
     public let id: Int
     public let name: String
     public let value: Int
-    public let characteristc: Characteristic
+    public let characteristic: Characteristic
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(value)
+        hasher.combine(characteristic)
+    }
 }
 
 extension Item {
     static let availableItems: [Item] = [
-        Item(id: 1, name: "Mad Scientist Brain", value: 6, characteristc: .collectionner),
-        Item(id: 2, name: "Pen", value: 3, characteristc: .collectionner),
-        Item(id: 3, name: "Chainsaw Hand", value: 4, characteristc: .extroverted),
-        Item(id: 4, name: "Pumpkin", value: 3, characteristc: .playful),
-        Item(id: 5, name: "Comfy Socks", value: 5, characteristc: .playful),
-        Item(id: 6, name: "Witch Hat", value: 6, characteristc: .extroverted),
+        Item(id: 1, name: "Mad Scientist Brain", value: 6, characteristic: .collectionner),
+        Item(id: 2, name: "Pen", value: 3, characteristic: .collectionner),
+        Item(id: 3, name: "Chainsaw Hand", value: 4, characteristic: .extroverted),
+        Item(id: 4, name: "Pumpkin", value: 3, characteristic: .playful),
+        Item(id: 5, name: "Comfy Socks", value: 5, characteristic: .playful),
+        Item(id: 6, name: "Witch Hat", value: 6, characteristic: .extroverted),
     ].shuffled()
 }
--- Sources/PitchABooServer/Entities/Base/Player.swift
+++ Sources/PitchABooServer/Entities/Base/Player.swift
@@ -7,17 +7,31 @@
 
 import Foundation
 
-public struct Player: Codable {
-    public let id: Int
-    public let name: String
-    public var bones: Int
-    public let sellingItem: Item
-    public let persona: Persona
+public struct Player: Codable, Hashable, Equatable {
+    let id: Int
+    let name: String
+    let bones: Int
+    let sellingItem: Item
+    let persona: Persona
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(bones)
+        hasher.combine(sellingItem)
+    }
+    
+    public static func ==(lhs: Player, rhs: Player) -> Bool {
+        return lhs.id == rhs.id &&
+            lhs.name == rhs.name &&
+            lhs.bones == rhs.bones &&
+            lhs.sellingItem == rhs.sellingItem
+    }
 }
 
 extension Player {
     static func createAnUndefinedPlayer() -> Player {
-        Player(id: 0, name: "Undefined", bones: 0, sellingItem: Item(id: 0, name: "None", value: 0, characteristc: .athletic), persona: Persona(id: 0, name: "None", characteristics: [.dumb]))
+        Player(id: 0, name: "Undefined", bones: 0, sellingItem: Item(id: 0, name: "None", value: 0, characteristic: .athletic), persona: Persona(id: 0, name: "None", characteristics: [.dumb]))
     }
 }
 
